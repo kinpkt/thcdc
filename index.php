bf1aece479d31935d7e8b917e6db8dec29e20ac7
thcdb <phakhinthon@gmail.com>
2021-01-18 12:25:54 +0700

    First deploy

---
+++ index.php
@@ -0,0 +1,9 @@
+<link rel="icon" href="Images/thailandcube.png">
+<link rel="stylesheet" href="CSS/fonts.css">
+<link rel="stylesheet" href="CSS/main.css">
+<body>
+<?php
+include("RSC/heading.html");
+include("RSC/index.html");
+?>
+</body>
