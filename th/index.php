d200bcdc7fea269a33f554b012e907b67b7cb70b
thcdb <phakhinthon@gmail.com>
2021-06-15 07:52:17 +0700

    Added THSarabun PSK

--- th/index.php
+++
@@ -1,8 +0,0 @@
-<link rel="icon" href="../Images/thailandcube.png">
-<link rel="stylesheet" href="../CSS/index.css">
-<body>
-<?php
-include("../RSC-th/heading.html");
-include("../RSC-th/index.html")
-?>
-</body>
